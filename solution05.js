b18e76d7fa4fc7e88702e2cd6148cb788834c228
Anik Haque <79439078+AnikHaque@users.noreply.github.com>
2023-08-04 23:16:22 +0600

    Update solution05.js

--- solution05.js
+++ solution05.js
@@ -1,1 +1,9 @@
-
+function confirmAction(question) {
+  return window.confirm(question);
+}
+const result = confirmAction("Do you want to proceed?");
+if (result) {
+  console.log("User clicked OK");
+} else {
+  console.log("User clicked Cancel");
+}
