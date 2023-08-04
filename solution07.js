1acae073fa82f16bf878ecaf5a5252d22a9c6125
Anik Haque <79439078+AnikHaque@users.noreply.github.com>
2023-08-04 23:17:12 +0600

    Update solution07.js

--- solution07.js
+++ solution07.js
@@ -1,1 +1,9 @@
+function toggleVisibility(elementId) {
+  const element = document.getElementById(elementId);
 
+  if (element.classList.contains('hidden')) {
+    element.classList.remove('hidden');
+  } else {
+    element.classList.add('hidden');
+  }
+}
