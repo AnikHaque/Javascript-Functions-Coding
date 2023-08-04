66d4bfb42387d569a71cfc270bad6becf7d2d7c5
Anik Haque <79439078+AnikHaque@users.noreply.github.com>
2023-08-04 23:16:41 +0600

    Update solution06.js

--- solution06.js
+++ solution06.js
@@ -1,1 +1,8 @@
+function showAlertOnHover(elementId, message) {
+  const element = document.getElementById(elementId);
+  element.addEventListener('mouseover', () => {
+    window.alert(message);
+  });
+}
 
+showAlertOnHover('hoverElement', 'You are hovering over me!');
